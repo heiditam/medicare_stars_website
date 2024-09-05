4a1e0927ffabbe1b3c9de640ae3d158fdfaf031e
Heidi Tam <125783723+heiditam@users.noreply.github.com>
2024-09-04 18:09:07 -0700

    sept 4 6:09 pm

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,25 +1,32 @@
 <script>
-	export let name;
+	// type stuff here
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<svg width='1900' height='250' xmlns="http://www.w3.org/2000/svg">
+		<rect width="100%" height="100%" style="fill:lightblue;" />
+		<text x= 450 y = 200 fill = 'navy' font-family='Latos' font-size= 75>Medicare Star Insights</text>
+	</svg>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
+
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		padding: 0;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: navy;
 		text-transform: uppercase;
+		font-family: 'Lato', sans-serif;
 		font-size: 4em;
 		font-weight: 100;
+		position: absolute;
+		text-align: center;
 	}
 
 	@media (min-width: 640px) {
